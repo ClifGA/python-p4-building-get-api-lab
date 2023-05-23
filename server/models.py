156484bd0575dd5087e8f64482305410229dbde9
Clifton Gockul <clifga@gmail.com>
2023-05-23 15:32:40 -0400

    completed lab

--- server/models.py
+++ server/models.py
@@ -11,10 +11,24 @@ db = SQLAlchemy(metadata=metadata)
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-bakedgoods.bakery',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    update_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
+
+    bakedgoods = db.relationship('BakedGood', backref='bakery')
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.bakedgoods',)
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    price = db.Column(db.Float)
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    update_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     
